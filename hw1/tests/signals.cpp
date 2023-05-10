7b5c0d0f431113641e8c1119a5a7a0e702094e70
MohamadWessam <mohamad.essa300@gmail.com>
2023-05-10 10:53:45 +0300

    added tests

---
+++ hw1/tests/signals.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <signal.h>
+#include "signals.h"
+#include "Commands.h"
+
+using namespace std;
+
+void ctrlZHandler(int sig_num) {
+  std::cout<<"smash: got ctrl-Z"<<std::endl;
+  SmallShell& smash = SmallShell::getInstance();
+  int currentProcess = smash.getCurrentProcess();
+  if(currentProcess > 0){
+    if(kill(currentProcess,SIGSTOP) == -1){
+      perror("smash error: kill failed");
+      return;
+    }
+    smash.addJob(currentProcess,smash.getCurrentCommand(),true);
+    smash.setCurrentProcess(-1);
+    smash.setCurrentCommand("");
+    smash.stopJob(currentProcess);
+    std::cout<<"smash: process "<<currentProcess<<" was stopped"<<std::endl;
+  }
+  return;
+}
+
+void ctrlCHandler(int sig_num) {
+  std::cout<<"smash: got ctrl-C"<<std::endl;
+  SmallShell& smash = SmallShell::getInstance();
+  int currentProcess = smash.getCurrentProcess();
+  int JID;
+  if(currentProcess > 0){
+    if(kill(currentProcess,SIGKILL) == -1){
+      perror("smash error: kill failed");
+      return;
+  }
+  smash.removeJob(smash.getJobJID(currentProcess));
+  std::cout<<"smash: process "<<currentProcess<<" was killed"<<std::endl;
+  }
+  return;
+}
+  
+
+
+
